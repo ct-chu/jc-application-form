22d0096d26bf122e6e660a3de910296ec77c65e6
ctchu <einsteinchu@gmail.com>
2025-05-19 20:48:13 +0800

    change var to let

--- app/form/page.tsx
+++ app/form/page.tsx
@@ -221,7 +221,7 @@ const formPagesConfig = [
 ];
 
 //labels for review page
-var labels = {
+let labels = {
   'schoolNameChn': "學校名稱（中文）",
   'schoolNameEng': "School name (ENG)",
   'isSpecial': "本校為教育局資助特殊學校。Our school is an aided special school.",
